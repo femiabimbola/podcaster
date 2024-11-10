d0fc9fc95eb67537ba0a76268f3c904b30eaab46
femi abimbola <femiabimbola6@gmail.com>
2024-11-10 01:34:07 -0800

    adding convex

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 
+interface PodcastCardProps {
+  imgURL: string;
+  title: string;
+  description: string;
+  podcastId: number;
+}
 
-export const PodcastCard = () => {
+export const PodcastCard = ({imgURL, title, description ,podcastId }:PodcastCardProps ) => {
   return (
-    <div>PodcastCard</div>
+    <div className="cursor-pointer">
+      <figure className="flex flex-col gap-2">
+        <Image src={imgURL} width={174} height={174} alt={title}
+          className="aspect-square h-fit w-full roundex-xl 2xl:size-[200px]"
+        />
+        <div className="flex flex-col">
+          <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
+          <h2 className="text-12 truncate font-normal capitalize text-white-4">{description}</h2>
+        </div>
+      </figure>
+      </div>
   )
 }
